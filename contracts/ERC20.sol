df4deba805e2d47c8d127c98094b641f85517a6b
Luke Park <twodude@naver.com>
2019-01-10 11:36:41 +0900

    Create ERC20.sol
    pragma solidity ^0.5.1;

---
+++ contracts/ERC20.sol
@@ -0,0 +1,13 @@
+pragma solidity ^0.5.1;
+
+contract ERC20 {
+  uint public totalSupply;
+  function balanceOf(address who) public view returns (uint);
+  function allowance(address owner, address spender) public view returns (uint);
+
+  function transfer(address to, uint value) public returns (bool ok);
+  function transferFrom(address from, address to, uint value) private returns (bool ok);
+  function approve(address spender, uint value) public returns (bool ok);
+  event Transfer(address indexed from, address indexed to, uint value);  
+  event Approval(address indexed owner, address indexed spender, uint value);
+}
